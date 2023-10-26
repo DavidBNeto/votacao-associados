3107fdf218e999d2e6e56177d8d7a2d184e5731c
DavidBNeto <netodavid25@hotmail.com>
2023-10-26 11:36:58 -0300

    add: autogenerated structure added

---
+++ src/main/java/com/davidbneto/votacao/VotacaoAssociadosApplication.java
@@ -0,0 +1,13 @@
+package com.davidbneto.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoAssociadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoAssociadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidbneto/votacao/VotacaoAssociadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidbneto.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoAssociadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
